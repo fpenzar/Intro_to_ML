b468a0efc525467f944f836fbced2afbaee8449d
Filip Penzar <fpenzar@gmail.com>
2023-09-13 17:43:13 +0200

    y in the standard notation

--- code/pca.py
+++ code/pca.py
@@ -2,7 +2,7 @@ from transform import *
 import matplotlib.pyplot as plt
 from scipy.linalg import svd
 
-X = X[:, 1:-1].astype(float)
+X = X[:, 1:].astype(float)
 
 # subtract the mean and standardize the data
 Y = X - np.ones((N, 1))*X.mean(0)
@@ -26,12 +26,11 @@ j = 1
 f = plt.figure()
 plt.title('PCA')
 #Z = array(Z)
-# for c in range(C):
+for c in range(C):
     # select indices belonging to class c:
-    # class_mask = y==c
-    # plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
-plt.plot(Z[:,i], Z[:,j], 'o', alpha=.5)
-# plt.legend(classNames)
+    class_mask = y==c
+    plt.plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+plt.legend(class_names)
 plt.xlabel('PC{0}'.format(i+1))
 plt.ylabel('PC{0}'.format(j+1))
 
@@ -52,16 +51,25 @@ plt.title('Variance explained')
 plt.show()
 
 # percent of the variance. Let's look at their coefficients:
-pcs = [i for i in range(6)]
+pcs = [i for i in range(10)]
 legendStrs = ['PC'+str(e+1) for e in pcs]
 bw = .2
-r = np.arange(2,M)
+r = np.arange(2,M+1)
+
+fig, ax = plt.subplots()
 for i in pcs:
-    plt.bar(r+i*bw, V[:,i], width=bw)
-plt.xticks(r+bw, attribute_names[1:-1],rotation=270)
-plt.xlabel('Attributes')
-plt.ylabel('Component coefficients')
-plt.legend(legendStrs)
-plt.grid()
+    ax.bar(r+i*bw, V[:,i], width=bw)
+ax.set_xticks(r+bw) 
+ax.set_xticklabels(attribute_names[1:],rotation=270)
+ax.set_xlabel('Attributes')
+ax.set_ylabel('Component coefficients')
+ax.legend(legendStrs, loc='upper left', bbox_to_anchor=(1, 1))
+ax.grid()
 plt.title('PCA Component Coefficients')
+# plt.xticks(r+bw, attribute_names[1:],rotation=270)
+# plt.xlabel('Attributes')
+# plt.ylabel('Component coefficients')
+# plt.legend(legendStrs)
+# plt.grid()
+# plt.title('PCA Component Coefficients')
 plt.show()
