d3cb77249e011771a49fcd94189d6d2b72c816f1
Filip Penzar <fpenzar@gmail.com>
2023-09-13 12:56:40 +0200

    Data cleanup

--- code/data.py
+++ code/data.py
@@ -27,7 +27,41 @@ cols = range(0, 35)
 X = raw_data[:, cols]
 
 # We can extract the attribute names that came from the header of the csv
-attributeNames = np.asarray(df.columns[cols])
+attribute_names_raw = np.asarray(df.columns[cols])
+
+# DATA cleanup
+
+# attributes we want to delete (see the report on why we remove each of them)
+attributes_to_remove = ["Armed Forces size", "Abbreviation", "Land Area(Km2)", "Calling Code", "Capital/Major City", "CPI Change (%)", "Co2-Emissions", "Currency-Code", "Largest city", 
+                        "Minimum wage", "Official language", "Out of pocket health expenditure", "Population: Labor force participation (%)", "Tax revenue (%)", 
+                        "Total tax rate", "Latitude", "Longitude"]
+
+# get the indices of the columns to remove
+indices = np.where(np.isin(attribute_names_raw, attributes_to_remove))
+
+# remove the columns from the dataset
+filtered_data = np.delete(X, indices, axis=1)
+attribute_names = np.delete(attribute_names_raw, indices)
+
+# index of the country
+country_index = np.where(np.isin(attribute_names, ["Country"]))
+
+p = 0
+# get a list of countries that have more than p percent null values
+def calculate_nan_perc_per_row(row):
+    nan_count = sum(np.isnan(x) if isinstance(x, (float, np.floating)) else False for x in row)
+    return nan_count / len(row)
+
+nan_percentages = [calculate_nan_perc_per_row(row) for row in filtered_data]
+indices_with_multiple_nans = np.where(np.array(nan_percentages) > p)[0]
+
+# get the country names of the deleted countries
+deleted_countries = filtered_data[indices_with_multiple_nans, country_index][0]
+print(deleted_countries)
+
+# Remove the countries with too many null attributes
+
+
 
 # We can determine the number of data objects and number of attributes using 
 # the shape of X
