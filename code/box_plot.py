e3e117e7cf4e0ee00692c9f24f990c04d6730ec1
Noa Margeta <nmargeta@gmail.com>
2023-09-20 20:21:42 +0200

    Add box plot by continents

---
+++ code/box_plot.py
@@ -0,0 +1,28 @@
+from matplotlib.pyplot import (figure, subplots, boxplot, title, xticks, ylim, 
+                               show, setp, sca)
+import numpy as np
+import pandas as pd
+from transform import *
+
+fig, ax = subplots(1,4, figsize=(20, 10))
+
+class_mask0 = (y==0) # binary mask to extract elements of class c
+class_mask1 = (y==1)
+class_mask2 = (y==2)
+class_mask3 = (y==3)
+class_mask4 = (y==4)
+class_mask5 = (y==5)
+
+attributes_to_plot = [3, 5, 11, 13]
+
+for i in range(4):
+    temp = [X[(y==0), attributes_to_plot[i]].astype(float),X[(y==1), attributes_to_plot[i]].astype(float),
+            X[(y==2), attributes_to_plot[i]].astype(float),X[(y==3), attributes_to_plot[i]].astype(float),
+            X[(y==4), attributes_to_plot[i]].astype(float),X[(y==5), attributes_to_plot[i]].astype(float)]
+    
+    ax[i].boxplot(temp)
+    ax[i].set_title(attribute_names[attributes_to_plot[i]])
+    ax[i].set_xticklabels(list(class_names.keys()), rotation=45)
+
+fig.tight_layout()
+show()
